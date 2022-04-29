3d8e9834fcfdf5523e46c6bb175af597312c28ea
tim10221 <ejrhgk16@naver.com>
2022-04-29 12:24:00 +0900

    no message

--- src/main/java/com/edonald/oauthConfig/NaverLogin.java
+++ src/main/java/com/edonald/oauthConfig/NaverLogin.java
@@ -1,13 +1,22 @@
 package com.edonald.oauthConfig;
 
+import java.io.IOException;
 import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
 import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
-
+@Component
 public class NaverLogin {
     private final static String CLIENT_ID = "DXUoYUTnxjQh2eIbHQ30";
     private final static String CLIENT_SECRET = "9ADIoX1_n_";
@@ -30,4 +39,28 @@ public class NaverLogin {
         		.build(NaverLoginApi.instance());	
         return oauthService.getAuthorizationUrl();
     }
+   
+    public OAuth2AccessToken getAccessToken(HttpSession session, String code, String state) throws IOException{
+    	String sessionState = (String)session.getAttribute(SESSION_STATE);
+    	 if (StringUtils.pathEquals(sessionState, state)) {
+    		 OAuth20Service oauthService = new ServiceBuilder().apiKey(CLIENT_ID).apiSecret(CLIENT_SECRET)
+                     .callback(REDIRECT_URI).state(state).build(NaverLoginApi.instance());
+    		  OAuth2AccessToken accessToken = oauthService.getAccessToken(code);
+              return accessToken;
+    	 }
+    	 return null;
+    }
+    
+    //Access Token을 이용하여 네이버 사용자 프로필 호출
+    public String getUserProfile(OAuth2AccessToken oauthToken) throws IOException {
+        OAuth20Service oauthService = new ServiceBuilder().apiKey(CLIENT_ID).apiSecret(CLIENT_SECRET)
+                .callback(REDIRECT_URI).build(NaverLoginApi.instance());
+        OAuthRequest request = new OAuthRequest(Verb.GET, PROFILE_API_URL, oauthService);
+        oauthService.signRequest(oauthToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
+    
+    
+    
 }
