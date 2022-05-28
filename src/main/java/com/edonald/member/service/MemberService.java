dcd7cdb509def34e8432744f888455c87d1cb6b1
YangWonJae <yang852039@naver.com>
2022-05-28 16:09:40 +0900

    no message

--- src/main/java/com/edonald/member/service/MemberService.java
+++ src/main/java/com/edonald/member/service/MemberService.java
@@ -24,7 +24,7 @@ public interface MemberService {
 	public String changeAccount(MemberDto sessionDto,MemberDto dto);
 	public void changeAccountByAdmin(MemberDto dto);
 	public String changePassword(MemberDto sessionDto,MemberDto dto,String checkPassword);
-	public void findPasswordByEmail(MemberDto dto);
+
 	public List<MemberDto> getMemberList(int user_status);
 	public void activityMemberControl(MemberDto dto);
 	public MemberDto getMemberById(String username);
